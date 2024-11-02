1c4c8665bc992f01375aa38f2bd7a6e66c01a22d
Youwen Wu <youwenw@gmail.com>
2024-11-01 22:57:10 -0700

    fix: remove unnecessary inputs.nixpkgs.follows

--- flake.nix
+++ flake.nix
@@ -8,10 +8,7 @@
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    crane = {
-      url = "github:ipetkov/crane";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    crane.url = "github:ipetkov/crane";
   };
 
   outputs =
