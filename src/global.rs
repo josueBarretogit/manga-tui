28e43e8af1cd9ba4301d89923af20dcdc88c1217
josuebarretogit <josuebarretogit@gmail.com>
2025-11-12 16:33:32 -0500

    add usr agent

--- src/global.rs
+++ src/global.rs
@@ -30,6 +30,7 @@ const USER_AGENTS: &[&str] = &[
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.5361.172 Safari/537.36",
     "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.5388.177 Safari/537.36",
     "Mozilla/5.0 (Macintosh; Intel Mac OS X 11_14) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.5397.215 Safari/537.36",
+    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36",
 ];
 
 #[inline]
@@ -48,6 +49,7 @@ pub mod test_utils {
     use std::error::Error;
 
     use crate::backend::tracker::{MangaTracker, PlanToReadArgs};
+    use crate::global::get_random_user_agent;
 
     /// NOTE: this should be under `manga_tracker`
     #[derive(Debug, Clone)]
